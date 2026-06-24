4cf0d4626e0dc4cf472ee9d8eb103ec0bc378046
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2026-06-24 11:29:51 -0600

    refactor: update developer activity panel and chain stats
    Integrate chain data hooks and refreshed GitHub activity rendering.

--- client/src/features/developer-activity/index.js
+++ client/src/features/developer-activity/index.js
@@ -1,3 +1,3 @@
 export { default as DeveloperActivity } from './DeveloperActivity'
-export { fetchChainActivity } from './githubActivity'
 export { fetchChainStats, formatTVL, formatCount, CHAIN_CONFIG } from './chainStats'
+export { fetchChainActivity } from './githubActivity'
